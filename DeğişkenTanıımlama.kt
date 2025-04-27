da2dd2613520586d1fcd8ba7e26132a7521207ff
2-0-0-5 <81965577+2-0-0-5@users.noreply.github.com>
2025-04-27 13:47:23 +0300

    Add files via upload

---
+++ DeğişkenTanıımlama.kt
@@ -0,0 +1,74 @@
+package com.kotlindersleri.bootcamp.ödev1
+
+fun main() {
+    val sehir = "Konya"
+    var ulke = "Türkiye"
+    val telefon = "05520325674"
+    var postaKodu = "332 396"
+    val email = "sumeyyeceyhan106@gmsil.com"
+    var meslek = "Bilgisayar Mühendisi"
+    val stokMiktari = 45
+    var musteriAdi = "Ekrem"
+    val bakiye = 20.50
+    var dogumTarihi = "15.11.2006"
+    val maas = 60.000
+    var medeniDurum ="Bekar"
+    val urunYorum = "Beklediğim gibi değil."
+    var odemeTarihi = "24.05.2025"
+    val odeme = 5.000
+    var siparisAdet = 10
+    var arabaModel = "Pegue"
+    val kitapAdi = "Tahir Sami Beyin Özel Hayatı"
+    var yayinlanmaTarihi = "13.11.2010"
+    val indirimMiktari = 0.50
+    var odaSayisi = 40
+    val  enlem = "34"
+    var boylam = "42"
+    val urunAdi = "Mont"
+    var yemekFiyati = 320
+    val marka = "frudia"
+    var muzikAdi = "Yepyeni destamlar yazma vakti"
+    val videoSuresi = "45:45"
+    var urunPuani = 4.5
+    val resimAdi = "Kaplumbağa terbiyecisi"
+    var dosyaFormati = "pdf"
+    val renk = "turuncu"
+    var renkKodu = "6384647"
+    var telefonModeli = "redmi not 11s"
+    val ekranBoyutu = "15 inç"
+    var agirlik = 45
+    val ulusalGun = "23 Nisan ulusal egemenlik ve çocuk bayramı"
+    var tatilGunu = "pazar"
+    val rezervasyonTarihi = "23.08.2025"
+    var sokakAdi = "Aslanlı Kışla"
+    val otobusHatti = "Km33 esenkentMetro"
+    var kalanDakika = 3
+    val takipKodu ="213134"
+    var kuponSuresi = "20 gün"
+    val kuponKodu = "64398"
+    var faturaAdresi = "kız yurdu"
+
+    println("Müşteri adı : $musteriAdi")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
